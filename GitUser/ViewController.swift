ae691ec186d21f1b1057319ad24d409565a686d4
Mhd Hasan Al Janadi <mhdhasanaljanadi@gmail.com>
2022-08-18 14:03:58 +0200

    Initial Commit

---
+++ GitUser/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitUser
+//
+//  Created by Mhd Hasan Al Janadi on 18/08/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
